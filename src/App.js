e821d750c3cef7d93972e42309015b62389924f4
keertheshwaran <keertheshwaran01@gmail.com>
2024-06-28 11:48:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Shop from './components/Shop';
-
+import { useSelector,useDispatch } from 'react-redux';
+import { increment,decrement } from './state/Counter/CounterSlice';
 function App() {
+  let count = useSelector((state)=>state.counter.value)
+  const dispatch =  useDispatch();
   return (
+    <>
+    
     <Shop/>
+    <div>
+      {count}
+    </div>
+    <button onClick={()=>dispatch(increment())}>plus</button>
+    <button onClick={()=>dispatch(decrement())}>minus</button>
+    
+    </>
   );
 }
 
